dbaced5cc698a87f193e1e39c2e168146366cdae
Larry <larrydblomme@hotmail.com>
2020-06-21 20:45:59 -0600

    follow _11's lead

--- src/main/java/org/laarryy/clippyv2/util/RoleUtil.java
+++ src/main/java/org/laarryy/clippyv2/util/RoleUtil.java
@@ -1,21 +1,13 @@
 package org.laarryy.clippyv2.util;
 
 import org.javacord.api.entity.message.Message;
-import org.javacord.api.entity.message.MessageAuthor;
-import org.javacord.api.entity.server.Server;
-import org.javacord.api.entity.user.User;
 import org.laarryy.clippyv2.Constants;
 
 public class RoleUtil {
-
-    public static boolean isStaffMention;
-
-    public RoleUtil(Message message, Server server) {
-        boolean mentionStaff = message.getMentionedUsers().get(0).getRoles(server).stream().anyMatch(role -> role.getIdAsString().equals(Constants.ROLE_STAFF));
-                if (mentionStaff) {
-                    isStaffMention = true;
-                }
-                else isStaffMention = false;
+    public static boolean hasStaffMention(Message message) {
+        if (!message.getServer().isPresent()) throw new IllegalStateException("Message does not have a server");
+        return message.getMentionedUsers().get(0).getRoles(message.getServer().get()).stream()
+                .anyMatch(role -> role.getIdAsString().equals(Constants.ROLE_STAFF));
     }
 }
 
