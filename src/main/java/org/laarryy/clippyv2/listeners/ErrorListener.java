633c4656e927cde0dcbce1e17688f14eda2a155f
underscore11code <minecrafter11mrt@gmail.com>
2020-06-24 01:15:54 -0700

    Hashmaps to the rescue!

--- src/main/java/org/laarryy/clippyv2/listeners/ErrorListener.java
+++ src/main/java/org/laarryy/clippyv2/listeners/ErrorListener.java
@@ -8,26 +8,30 @@ import org.laarryy.clippyv2.Main;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.regex.*;
 import java.util.regex.Pattern;
 
 public class ErrorListener implements MessageCreateListener {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
-    private static final Pattern[] pastebins = {
-            Pattern.compile("https?://hastebin\\.com/(\\w+)(?:\\.\\w+)?/g"),
-            Pattern.compile("https?://hasteb\\.in/(\\w+)(?:\\.\\w+)?/g"),
-            Pattern.compile("https?://paste\\.helpch\\.at/(\\w+)(?:\\.\\w+)?/g"),
-            Pattern.compile("https?://bytebin\\.lucko\\.me/(\\w+)/g"),
-            Pattern.compile("https?://paste\\.lucko\\.me/(\\w+)(?:\\.\\w+)?/g"),
-            Pattern.compile("https?://pastebin\\.com/(\\w+)(?:\\.\\w+)?/g"),
-            Pattern.compile("https?://gist\\.github\\.com/(\\w+/\\w+)(?:\\.\\w+/\\w+)?/g"),
-            Pattern.compile("https?://gitlab\\.com/snippets/(\\w+)(?:\\.\\w+)?/g")
-    };
+    private final HashMap<Pattern, String> pastebins = new HashMap<>();
+    
+    public ErrorListener() {
+        pastebins.put(Pattern.compile("https?://hastebin\\.com/(\\w+)(?:\\.\\w+)?"), "https://hastebin.com/raw/{code}");
+        pastebins.put(Pattern.compile("https?://hasteb\\.in/(\\w+)(?:\\.\\w+)?"), "https://hasteb.in/raw/{code}");
+        pastebins.put(Pattern.compile("https?://paste\\.helpch\\.at/(\\w+)(?:\\.\\w+)?"), "https://paste.helpch.at/raw/{code}");
+        pastebins.put(Pattern.compile("https?://bytebin\\.lucko\\.me/(\\w+)"), "https://bytebin.lucko.me/{code}");
+        pastebins.put(Pattern.compile("https?://paste\\.lucko\\.me/(\\w+)(?:\\.\\w+)?"), "https://paste.lucko.me/raw/{code}");
+        pastebins.put(Pattern.compile("https?://pastebin\\.com/(\\w+)(?:\\.\\w+)?"), "https://pastebin.com/raw/{code}");
+        pastebins.put(Pattern.compile("https?://gist\\.github\\.com/(\\w+/\\w+)(?:\\.\\w+/\\w+)?"), "https://gist.github.com/{code}/raw/");
+        pastebins.put(Pattern.compile("https?://gitlab\\.com/snippets/(\\w+)(?:\\.\\w+)?"), "https://gitlab.com/snippets/{code}/raw");
+    }
 
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
-        for (Pattern pastebin : pastebins) {
+        for (Pattern pastebin : pastebins.keySet()) {
             // All logic goes here!
+            // pastebins.get(pastebin) to get the GET URL
         }
     }
 }
