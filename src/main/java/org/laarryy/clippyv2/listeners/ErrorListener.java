61c5935f5e5e376ce794be47852c56bb3cad84a2
underscore11code <minecrafter11mrt@gmail.com>
2020-06-24 02:20:51 -0700

    Does this work?

--- src/main/java/org/laarryy/clippyv2/listeners/ErrorListener.java
+++ src/main/java/org/laarryy/clippyv2/listeners/ErrorListener.java
@@ -4,13 +4,16 @@ package org.laarryy.clippyv2.listeners;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 import org.laarryy.clippyv2.Main;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.awt.*;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.regex.*;
 import java.util.regex.Pattern;
@@ -19,6 +22,7 @@ public class ErrorListener implements MessageCreateListener {
     private static final OkHttpClient client = new OkHttpClient();
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
     private final HashMap<Pattern, String> pastebins = new HashMap<>();
+    private final ArrayList<Check> checks = new ArrayList<>();
 
     public ErrorListener() {
         pastebins.put(Pattern.compile("https?://hastebin\\.com/(\\w+)(?:\\.\\w+)?"), "https://hastebin.com/raw/{code}");
@@ -29,6 +33,14 @@ public class ErrorListener implements MessageCreateListener {
         pastebins.put(Pattern.compile("https?://pastebin\\.com/(\\w+)(?:\\.\\w+)?"), "https://pastebin.com/raw/{code}");
         pastebins.put(Pattern.compile("https?://gist\\.github\\.com/(\\w+/\\w+)(?:\\.\\w+/\\w+)?"), "https://gist.github.com/{code}/raw/");
         pastebins.put(Pattern.compile("https?://gitlab\\.com/snippets/(\\w+)(?:\\.\\w+)?"), "https://gitlab.com/snippets/{code}/raw");
+
+        checks.add(new Check("MySQL exceeded max connections",
+                "https://github.com/lucko/LuckPerms/wiki/Storage-system-errors#mysql-exceeded-max-connections",
+                Pattern.compile("Caused by: com\\.mysql\\.jdbc\\.exceptions\\.jdbc4\\.MySQLSyntaxErrorException: User '\\w+' has exceeded the 'max_user_connections' resource \\(current value: \\w+\\)")));
+        checks.add(new Check("MySQL \"No Operations allowed after connection closed\" error",
+                "https://github.com/lucko/LuckPerms/wiki/Storage-system-errors#mysql-no-operations-allowed-after-connection-closed-error",
+                Pattern.compile("me\\.lucko\\.luckperms\\.lib\\.hikari\\.pool\\.PoolBase - luckperms-hikari - Failed to validate connection me\\.lucko\\.luckperms\\.lib\\.mysql\\.jdbc\\.JDBC4Connection@\\w+ \\(No operations allowed after connection closed\\.\\)"),
+                Pattern.compile("me\\.lucko\\.luckperms\\.lib\\.hikari\\.pool\\.PoolBase - luckperms-hikari- Failed to validate connection me\\.lucko\\.luckperms\\.lib\\.mariadb\\.MariaDbConnection@\\w+ \\(\\w+ cannot be called on a closed connection\\)")));
     }
 
     @Override
@@ -50,9 +62,46 @@ public class ErrorListener implements MessageCreateListener {
                 if (!response.isSuccessful()) continue;
                 if (response.body() == null) continue;
                 logger.debug(response.body().string());
+                for (Check check : checks) {
+                    boolean matched = false;
+                    for (Pattern regex : check.getPatterns())
+                        if (regex.matcher(response.body().string()).matches())
+                            matched = true;
+
+                    if (!matched) continue;
+                    event.getChannel().sendMessage(new EmbedBuilder()
+                            .setTitle(check.getTitle())
+                            .addField("Read more:", check.getUrl())
+                            .setFooter(matcher.group() + " | Sent by " + event.getMessageAuthor().getDisplayName())
+                            .setColor(Color.RED));
+                }
             } catch (IOException e) {
                 logger.warn(e.getMessage());
             }
+        }
+    }
+
+    static class Check {
+        private final Pattern[] patterns;
+        private final String title;
+        private final String url;
+
+        public Check(String title, String url, Pattern... patterns) {
+            this.patterns = patterns;
+            this.title = title;
+            this.url = url;
+        }
+
+        public Pattern[] getPatterns() {
+            return patterns;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getUrl() {
+            return url;
         }
     }
 }
