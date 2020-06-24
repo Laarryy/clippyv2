8b244c6145283e7af767bdc6385253e59862f913
underscore11code <minecrafter11mrt@gmail.com>
2020-06-23 19:23:12 -0700

    fix up

--- src/main/java/org/laarryy/clippyv2/listeners/AutoUploadListener.java
+++ src/main/java/org/laarryy/clippyv2/listeners/AutoUploadListener.java
@@ -1,5 +1,7 @@
 package org.laarryy.clippyv2.listeners;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 import org.laarryy.clippyv2.Main;
@@ -8,21 +10,30 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Marker;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.Format;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class AutoUploadListener implements MessageCreateListener {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    private static final OkHttpClient client = new OkHttpClient();
 
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
-        if (!event.getMessageAuthor().isUser())
-        return;
+        if (!event.getMessageAuthor().isUser()) return;
 
-        if (event.getMessage().getAttachments().contains(1));
+        if (event.getMessage().getAttachments().size() == 0) return;
 
-        logger.debug(String.valueOf(event.getMessageAttachments()));
+        event.getMessage().getAttachments().forEach(messageAttachment -> {
+            if (messageAttachment.isImage()) return;
+            Request request = new Request.Builder().url(messageAttachment.getUrl()).build();
+            try {
+                logger.debug(client.newCall(request).execute().body().string()); // TODO ensure body != null
+            } catch (IOException ioException) {
+                logger.warn("IOException while attempting to reach " + messageAttachment.getUrl() + ": " + ioException.getMessage());
+            }
+        });
 
     }
 }
