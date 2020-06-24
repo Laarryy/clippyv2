8ee6bab08b93cb20f79fe6ee3e05fa043f218aaf
underscore11code <minecrafter11mrt@gmail.com>
2020-06-23 20:04:48 -0700

    broken code

--- src/main/java/org/laarryy/clippyv2/listeners/AutoUploadListener.java
+++ src/main/java/org/laarryy/clippyv2/listeners/AutoUploadListener.java
@@ -1,7 +1,6 @@
 package org.laarryy.clippyv2.listeners;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
+import okhttp3.*;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 import org.laarryy.clippyv2.Main;
@@ -18,6 +17,7 @@ import java.util.regex.Pattern;
 public class AutoUploadListener implements MessageCreateListener {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
     private static final OkHttpClient client = new OkHttpClient();
+    private static final String BYTEBIN_URL = "https://bytebin.lucko.me";
 
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
@@ -29,9 +29,19 @@ public class AutoUploadListener implements MessageCreateListener {
             if (messageAttachment.isImage()) return;
             Request request = new Request.Builder().url(messageAttachment.getUrl()).build();
             try {
-                logger.debug(client.newCall(request).execute().body().string()); // TODO ensure body != null
+                Response response = client.newCall(request).execute();
+                if (!response.isSuccessful()) return;
+                if (response.body() == null) return;
+                //noinspection ConstantConditions
+                RequestBody requestBody = RequestBody.create(MediaType.get(response.header("Content-Type")), response.body().string());
+                Request postRequest = new Request.Builder().post(requestBody).url(BYTEBIN_URL + "/post").build();
+                Response bytebinResponse = client.newCall(postRequest).execute();
+                if (!response.isSuccessful()) return;
+                // TODO MAKE WORK
+                event.getChannel().sendMessage(BYTEBIN_URL + "/" + response.header("Location", "null")).join();
             } catch (IOException ioException) {
-                logger.warn("IOException while attempting to reach " + messageAttachment.getUrl() + ": " + ioException.getMessage());
+                logger.warn("IOException while attempting to upload a file to bytebin:");
+                ioException.printStackTrace();
             }
         });
 
