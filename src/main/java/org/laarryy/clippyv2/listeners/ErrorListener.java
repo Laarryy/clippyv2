08ff7f8e6b2e5ed06c7a673e4a43a69f8ab4323c
underscore11code <minecrafter11mrt@gmail.com>
2020-06-24 00:45:31 -0700

    Make loopable

--- src/main/java/org/laarryy/clippyv2/listeners/ErrorListener.java
+++ src/main/java/org/laarryy/clippyv2/listeners/ErrorListener.java
@@ -13,33 +13,21 @@ import java.util.regex.Pattern;
 
 public class ErrorListener implements MessageCreateListener {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
-    private static final Pattern hastebin1 = Pattern.compile(".*https?://hastebin.com.*");
-    private static final Pattern hastebin2 = Pattern.compile("https?://hasteb\\.in/(\\w+)(?:\\.\\w+)?/g");
-    private static final Pattern helpchat = Pattern.compile("https?://paste\\.helpch\\.at/(\\w+)(?:\\.\\w+)?/g");
-    private static final Pattern bytebin = Pattern.compile("https?://bytebin\\.lucko\\.me/(\\w+)/g");
-    private static final Pattern paste = Pattern.compile("https?://paste\\.lucko\\.me/(\\w+)(?:\\.\\w+)?/g");
-    private static final Pattern pastebin = Pattern.compile("https?://pastebin\\.com/(\\w+)(?:\\.\\w+)?/g");
-    private static final Pattern gist = Pattern.compile("https?://gist\\.github\\.com/(\\w+/\\w+)(?:\\.\\w+/\\w+)?/g");
-    private static final Pattern gitlab = Pattern.compile("https?://gitlab\\.com/snippets/(\\w+)(?:\\.\\w+)?/g");
+    private static final Pattern[] pastebins = {
+            Pattern.compile("https?://hastebin\\.com/(\\w+)(?:\\.\\w+)?/g"),
+            Pattern.compile("https?://hasteb\\.in/(\\w+)(?:\\.\\w+)?/g"),
+            Pattern.compile("https?://paste\\.helpch\\.at/(\\w+)(?:\\.\\w+)?/g"),
+            Pattern.compile("https?://bytebin\\.lucko\\.me/(\\w+)/g"),
+            Pattern.compile("https?://paste\\.lucko\\.me/(\\w+)(?:\\.\\w+)?/g"),
+            Pattern.compile("https?://pastebin\\.com/(\\w+)(?:\\.\\w+)?/g"),
+            Pattern.compile("https?://gist\\.github\\.com/(\\w+/\\w+)(?:\\.\\w+/\\w+)?/g"),
+            Pattern.compile("https?://gitlab\\.com/snippets/(\\w+)(?:\\.\\w+)?/g")
+    };
 
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
-        CharSequence contents = event.getMessage().getContent();
-            boolean hastebin1match = Pattern.matches(hastebin1.toString(), contents);
-
-            while (event.getMessage().getContent() != null) {
-                contents.
-            }
-
-
-        if (event.getMessageAuthor().isUser())
-            if (String.valueOf(hastebin1match).equals("true")) {
-                Request request = new Request.Builder()
-                        .url(String.valueOf(hastebin1match))
-                        .build();
-
-
-            logger.debug(String.valueOf(hastebin1match));
+        for (Pattern pastebin : pastebins) {
+            // All logic goes here!
         }
     }
 }
