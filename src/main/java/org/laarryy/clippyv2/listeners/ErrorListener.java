a9463dbac81747944f500fcda6c03b57f415fa85
Larry <larrydblomme@hotmail.com>
2020-06-25 01:18:22 -0600

    hopefully fix all regex

--- src/main/java/org/laarryy/clippyv2/listeners/ErrorListener.java
+++ src/main/java/org/laarryy/clippyv2/listeners/ErrorListener.java
@@ -37,20 +37,20 @@ public class ErrorListener implements MessageCreateListener {
 
         checks.add(new Check("MySQL exceeded max connections",
                 "https://github.com/lucko/LuckPerms/wiki/Storage-system-errors#mysql-exceeded-max-connections",
-                Pattern.compile("(?:.*)?Caused by: com\\.mysql\\.jdbc\\.exceptions\\.jdbc4\\.MySQLSyntaxErrorException: User '\\w+' has exceeded the 'max_user_connections' resource \\(current value: \\w+\\)(?:.*)?")));
+                Pattern.compile(("(?:.*)?Caused by: com\\.mysql\\.jdbc\\.exceptions\\.jdbc4\\.MySQLSyntaxErrorException: User '.*' has exceeded the 'max_user_connections' resource \\(current value: \\w+\\)(?:.*)?"), Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE)));
         checks.add(new Check("MySQL \"No Operations allowed after connection closed\" error",
                 "https://github.com/lucko/LuckPerms/wiki/Storage-system-errors#mysql-no-operations-allowed-after-connection-closed-error",
-                Pattern.compile("(?:.*)?me\\.lucko\\.luckperms\\.lib\\.hikari\\.pool\\.PoolBase - luckperms-hikari - Failed to validate connection me\\.lucko\\.luckperms\\.lib\\.mysql\\.jdbc\\.JDBC4Connection@\\w+ \\(No operations allowed after connection closed\\.\\)(?:.*)?"),
-                Pattern.compile("(?:.*)?me\\.lucko\\.luckperms\\.lib\\.hikari\\.pool\\.PoolBase - luckperms-hikari- Failed to validate connection me\\.lucko\\.luckperms\\.lib\\.mariadb\\.MariaDbConnection@\\w+ \\(\\w+ cannot be called on a closed connection\\)(?:.*)?")));
+                Pattern.compile(("(?:.*)?me\\.lucko\\.luckperms\\.lib\\.hikari\\.pool\\.PoolBase - luckperms-hikari - Failed to validate connection me\\.lucko\\.luckperms\\.lib\\.mysql\\.jdbc\\.JDBC4Connection@\\w+ \\(No operations allowed after connection closed\\.\\)(?:.*)?"), Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE),
+                Pattern.compile(("(?:.*)?me\\.lucko\\.luckperms\\.lib\\.hikari\\.pool\\.PoolBase - luckperms-hikari- Failed to validate connection me\\.lucko\\.luckperms\\.lib\\.mariadb\\.MariaDbConnection@\\w+ \\(\\w+ cannot be called on a closed connection\\)(?:.*)?"), Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE)));
         checks.add(new Check("LuckPerms cannot connect to your MySQL server",
                 "https://github.com/lucko/LuckPerms/wiki/Storage-system-errors#luckperms-cannot-connect-to-my-mysql-server",
-                Pattern.compile("(?:.*)?java\\.util\\.concurrent\\.CompletionException: java\\.sql\\.SQLTransientConnectionException: luckperms(?:-hikari)? - Connection is not available, request timed out after(?:.*)?"),
-                Pattern.compile("(?:.*)?java\\.sql\\.SQLTransientConnectionException: luckperms(?:-hikari)? - Connection is not available, request timed out after (?:.*)?"),
-                Pattern.compile("(?:.*)?luckperms - Failed to validate connection com\\.mysql\\.jdbc\\.JDBC4Connection@\\w+ \\(Communications link failure\\)(?:.*)?"),
-                Pattern.compile("(?:.*)?The last packet successfully received from the server was \\d+ milliseconds ago\\. The last packet sent successfully to the server was \\d+ milliseconds ago\\.(?:.*)?")));
+                Pattern.compile(("(?:.*)?java\\.util\\.concurrent\\.CompletionException: java\\.sql\\.SQLTransientConnectionException: luckperms(?:-hikari)? - Connection is not available, request timed out after(?:.*)?"), Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE),
+                Pattern.compile(("(?:.*)?java\\.sql\\.SQLTransientConnectionException: luckperms(?:-hikari)? - Connection is not available, request timed out after (?:.*)?"), Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE),
+                Pattern.compile(("(?:.*)?luckperms - Failed to validate connection com\\.mysql\\.jdbc\\.JDBC4Connection@\\w+ \\(Communications link failure\\)(?:.*)?"), Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE),
+                Pattern.compile(("(?:.*)?The last packet successfully received from the server was \\d+ milliseconds ago\\. The last packet sent successfully to the server was \\d+ milliseconds ago\\.(?:.*)?"), Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE)));
         checks.add(new Check("MySQL SSL Error",
                 "https://github\\.com/lucko/LuckPerms/wiki/Storage-system-errors#mysql-ssl-errors",
-                Pattern.compile((".*?Establishing SSL connection without server's identity verification is not recommended.*?"))));
+                Pattern.compile(("(?:.*)?(Establishing SSL connection without server's identity verification is not recommended)(?:.*)?"), Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE)));
     }
 
 
