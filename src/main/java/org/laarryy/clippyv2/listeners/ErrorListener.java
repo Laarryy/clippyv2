9bb9f6e02dc7e3da7926563a345502faca9888aa
Larry <larrydblomme@hotmail.com>
2020-06-25 02:29:58 -0600

    finishing touches on errorlistener

--- src/main/java/org/laarryy/clippyv2/listeners/ErrorListener.java
+++ src/main/java/org/laarryy/clippyv2/listeners/ErrorListener.java
@@ -26,6 +26,7 @@ public class ErrorListener implements MessageCreateListener {
     private final ArrayList<Check> checks = new ArrayList<>();
 
     public ErrorListener() {
+        // mcpaste.io will not work, because it has a massive delay on actually sending data
         pastebins.put(Pattern.compile("(?:.*)?https?://hastebin\\.com/(\\w+)(?:.*)?"), "https://hastebin.com/raw/{code}");
         pastebins.put(Pattern.compile("(?:.*)?https?://hasteb\\.in/(\\w+)(?:.*)?"), "https://hasteb.in/raw/{code}");
         pastebins.put(Pattern.compile("(?:.*)?https?://paste\\.helpch\\.at/(\\w+)(?:.*)?"), "https://paste.helpch.at/raw/{code}");
@@ -34,25 +35,32 @@ public class ErrorListener implements MessageCreateListener {
         pastebins.put(Pattern.compile("(?:.*)?https?://pastebin\\.com/(\\w+)(?:.*)?"), "https://pastebin.com/raw/{code}");
         pastebins.put(Pattern.compile("(?:.*)?https?://gist\\.github\\.com/(\\w+/\\w+)(?:.*)?"), "https://gist.github.com/{code}/raw/");
         pastebins.put(Pattern.compile("(?:.*)?https?://gitlab\\.com/snippets/(\\w+)(?:.*)?"), "https://gitlab.com/snippets/{code}/raw");
+        pastebins.put(Pattern.compile("(?:.*)?https?://paste\\.helpch\\.at/(\\w+)(?:.*)?"), "https://paste.helpch.at/{code}");
 
-        checks.add(new Check("MySQL exceeded max connections",
+
+        // Add new error checks here
+        // and make sure that less specific checks are last,
+        // especially the very broad check for "cannot connect to mysql server"
+        // because checks stop once the first successful one is found, in order
+        // to stop embed spam for every single check on a log.
+
+        checks.add(new Check("MySQL Exceeded Max Connections",
                 "https://github.com/lucko/LuckPerms/wiki/Storage-system-errors#mysql-exceeded-max-connections",
                 Pattern.compile(("(?:.*)?Caused by: com\\.mysql\\.jdbc\\.exceptions\\.jdbc4\\.MySQLSyntaxErrorException: User '.*' has exceeded the 'max_user_connections' resource \\(current value: \\w+\\)(?:.*)?"), Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE)));
         checks.add(new Check("MySQL \"No Operations allowed after connection closed\" error",
                 "https://github.com/lucko/LuckPerms/wiki/Storage-system-errors#mysql-no-operations-allowed-after-connection-closed-error",
                 Pattern.compile(("(?:.*)?me\\.lucko\\.luckperms\\.lib\\.hikari\\.pool\\.PoolBase - luckperms-hikari - Failed to validate connection me\\.lucko\\.luckperms\\.lib\\.mysql\\.jdbc\\.JDBC4Connection@\\w+ \\(No operations allowed after connection closed\\.\\)(?:.*)?"), Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE),
                 Pattern.compile(("(?:.*)?me\\.lucko\\.luckperms\\.lib\\.hikari\\.pool\\.PoolBase - luckperms-hikari- Failed to validate connection me\\.lucko\\.luckperms\\.lib\\.mariadb\\.MariaDbConnection@\\w+ \\(\\w+ cannot be called on a closed connection\\)(?:.*)?"), Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE)));
-        checks.add(new Check("LuckPerms cannot connect to your MySQL server",
+        checks.add(new Check("MySQL SSL Error",
+                "https://github\\.com/lucko/LuckPerms/wiki/Storage-system-errors#mysql-ssl-errors",
+                Pattern.compile(("(?:.*)?(Establishing SSL connection without server's identity verification is not recommended)(?:.*)?"), Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE)));
+        checks.add(new Check("LuckPerms cannot connect to your database server",
                 "https://github.com/lucko/LuckPerms/wiki/Storage-system-errors#luckperms-cannot-connect-to-my-mysql-server",
                 Pattern.compile(("(?:.*)?java\\.util\\.concurrent\\.CompletionException: java\\.sql\\.SQLTransientConnectionException: luckperms(?:-hikari)? - Connection is not available, request timed out after(?:.*)?"), Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE),
                 Pattern.compile(("(?:.*)?java\\.sql\\.SQLTransientConnectionException: luckperms(?:-hikari)? - Connection is not available, request timed out after (?:.*)?"), Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE),
                 Pattern.compile(("(?:.*)?luckperms - Failed to validate connection com\\.mysql\\.jdbc\\.JDBC4Connection@\\w+ \\(Communications link failure\\)(?:.*)?"), Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE),
                 Pattern.compile(("(?:.*)?The last packet successfully received from the server was \\d+ milliseconds ago\\. The last packet sent successfully to the server was \\d+ milliseconds ago\\.(?:.*)?"), Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE)));
-        checks.add(new Check("MySQL SSL Error",
-                "https://github\\.com/lucko/LuckPerms/wiki/Storage-system-errors#mysql-ssl-errors",
-                Pattern.compile(("(?:.*)?(Establishing SSL connection without server's identity verification is not recommended)(?:.*)?"), Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE)));
     }
-
 
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
@@ -86,6 +94,7 @@ public class ErrorListener implements MessageCreateListener {
                             .addField("Read more:", check.getUrl())
                             .setFooter(matcher.group() + " | Sent by " + event.getMessageAuthor().getDisplayName())
                             .setColor(Color.RED));
+                    return;
                 }
             } catch (IOException | NullPointerException e) {
                 logger.warn(e.getMessage());
