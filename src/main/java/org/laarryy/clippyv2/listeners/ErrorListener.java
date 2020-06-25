cdb566751b50dd1d241420cdfedfe4794f6cba46
Larry <larrydblomme@hotmail.com>
2020-06-24 23:38:00 -0600

    start on fix regex

--- src/main/java/org/laarryy/clippyv2/listeners/ErrorListener.java
+++ src/main/java/org/laarryy/clippyv2/listeners/ErrorListener.java
@@ -26,31 +26,31 @@ public class ErrorListener implements MessageCreateListener {
     private final ArrayList<Check> checks = new ArrayList<>();
 
     public ErrorListener() {
-        pastebins.put(Pattern.compile("https?://hastebin\\.com/(\\w+)(?:\\.\\w+)?"), "https://hastebin.com/raw/{code}");
-        pastebins.put(Pattern.compile("https?://hasteb\\.in/(\\w+)(?:\\.\\w+)?"), "https://hasteb.in/raw/{code}");
-        pastebins.put(Pattern.compile("https?://paste\\.helpch\\.at/(\\w+)(?:\\.\\w+)?"), "https://paste.helpch.at/raw/{code}");
-        pastebins.put(Pattern.compile("https?://bytebin\\.lucko\\.me/(\\w+)"), "https://bytebin.lucko.me/{code}");
-        pastebins.put(Pattern.compile("https?://paste\\.lucko\\.me/(\\w+)(?:\\.\\w+)?"), "https://paste.lucko.me/raw/{code}");
-        pastebins.put(Pattern.compile("https?://pastebin\\.com/(\\w+)(?:\\.\\w+)?"), "https://pastebin.com/raw/{code}");
-        pastebins.put(Pattern.compile("https?://gist\\.github\\.com/(\\w+/\\w+)(?:\\.\\w+/\\w+)?"), "https://gist.github.com/{code}/raw/");
-        pastebins.put(Pattern.compile("https?://gitlab\\.com/snippets/(\\w+)(?:\\.\\w+)?"), "https://gitlab.com/snippets/{code}/raw");
+        pastebins.put(Pattern.compile("(?:\\.\\w*)?https?://hastebin\\.com/(\\w+)(?:\\.\\w*)?"), "https://hastebin.com/raw/{code}");
+        pastebins.put(Pattern.compile("(?:\\.\\w*)?https?://hasteb\\.in/(\\w+)(?:\\.\\w*)?"), "https://hasteb.in/raw/{code}");
+        pastebins.put(Pattern.compile("(?:\\.\\w*)?https?://paste\\.helpch\\.at/(\\w+)(?:\\.\\w*)?"), "https://paste.helpch.at/raw/{code}");
+        pastebins.put(Pattern.compile("(?:\\.\\w*)?https?://bytebin\\.lucko\\.me/(\\w+)"), "https://bytebin.lucko.me/{code}");
+        pastebins.put(Pattern.compile("(?:\\.\\w*)?https?://paste\\.lucko\\.me/(\\w+)(?:\\.\\w*)?"), "https://paste.lucko.me/raw/{code}");
+        pastebins.put(Pattern.compile("(?:\\.\\w*)?https?://pastebin\\.com/(\\w+)(?:\\.\\w*)?"), "https://pastebin.com/raw/{code}");
+        pastebins.put(Pattern.compile("(?:\\.\\w*)?https?://gist\\.github\\.com/(\\w+/\\w+)(?:\\.\\w+/\\w*)?"), "https://gist.github.com/{code}/raw/");
+        pastebins.put(Pattern.compile("(?:\\.\\w*)?https?://gitlab\\.com/snippets/(\\w+)(?:\\.\\w*)?"), "https://gitlab.com/snippets/{code}/raw");
 
         checks.add(new Check("MySQL exceeded max connections",
                 "https://github.com/lucko/LuckPerms/wiki/Storage-system-errors#mysql-exceeded-max-connections",
-                Pattern.compile("Caused by: com\\.mysql\\.jdbc\\.exceptions\\.jdbc4\\.MySQLSyntaxErrorException: User '\\w+' has exceeded the 'max_user_connections' resource \\(current value: \\w+\\)")));
+                Pattern.compile("(?:\\.\\w*)?Caused by: com\\.mysql\\.jdbc\\.exceptions\\.jdbc4\\.MySQLSyntaxErrorException: User '\\w+' has exceeded the 'max_user_connections' resource \\(current value: \\w+\\)(?:\\.\\w*)?")));
         checks.add(new Check("MySQL \"No Operations allowed after connection closed\" error",
                 "https://github.com/lucko/LuckPerms/wiki/Storage-system-errors#mysql-no-operations-allowed-after-connection-closed-error",
-                Pattern.compile("me\\.lucko\\.luckperms\\.lib\\.hikari\\.pool\\.PoolBase - luckperms-hikari - Failed to validate connection me\\.lucko\\.luckperms\\.lib\\.mysql\\.jdbc\\.JDBC4Connection@\\w+ \\(No operations allowed after connection closed\\.\\)"),
-                Pattern.compile("me\\.lucko\\.luckperms\\.lib\\.hikari\\.pool\\.PoolBase - luckperms-hikari- Failed to validate connection me\\.lucko\\.luckperms\\.lib\\.mariadb\\.MariaDbConnection@\\w+ \\(\\w+ cannot be called on a closed connection\\)")));
+                Pattern.compile("(?:\\.\\w*)?me\\.lucko\\.luckperms\\.lib\\.hikari\\.pool\\.PoolBase - luckperms-hikari - Failed to validate connection me\\.lucko\\.luckperms\\.lib\\.mysql\\.jdbc\\.JDBC4Connection@\\w+ \\(No operations allowed after connection closed\\.\\)(?:\\.\\w*)?"),
+                Pattern.compile("(?:\\.\\w*)?me\\.lucko\\.luckperms\\.lib\\.hikari\\.pool\\.PoolBase - luckperms-hikari- Failed to validate connection me\\.lucko\\.luckperms\\.lib\\.mariadb\\.MariaDbConnection@\\w+ \\(\\w+ cannot be called on a closed connection\\)(?:\\.\\w*)?")));
         checks.add(new Check("LuckPerms cannot connect to your MySQL server",
                 "https://github.com/lucko/LuckPerms/wiki/Storage-system-errors#luckperms-cannot-connect-to-my-mysql-server",
-                Pattern.compile("java\\.util\\.concurrent\\.CompletionException: java\\.sql\\.SQLTransientConnectionException: luckperms(?:-hikari)? - Connection is not available, request timed out after \\d+ms\\."),
-                Pattern.compile("java\\.sql\\.SQLTransientConnectionException: luckperms(?:-hikari)? - Connection is not available, request timed out after \\d+ms\\."),
-                Pattern.compile("luckperms - Failed to validate connection com\\.mysql\\.jdbc\\.JDBC4Connection@\\w+ \\(Communications link failure\\)"),
-                Pattern.compile("The last packet successfully received from the server was \\d+ milliseconds ago\\. The last packet sent successfully to the server was \\d+ milliseconds ago\\.")));
+                Pattern.compile("(?:\\.\\w*)?java\\.util\\.concurrent\\.CompletionException: java\\.sql\\.SQLTransientConnectionException: luckperms(?:-hikari)? - Connection is not available, request timed out after \\d+ms\\.(?:\\.\\w*)?"),
+                Pattern.compile("(?:\\.\\w*)?java\\.sql\\.SQLTransientConnectionException: luckperms(?:-hikari)? - Connection is not available, request timed out after \\d+ms\\.(?:\\.\\w*)?"),
+                Pattern.compile("(?:\\.\\w*)?luckperms - Failed to validate connection com\\.mysql\\.jdbc\\.JDBC4Connection@\\w+ \\(Communications link failure\\)(?:\\.\\w*)?"),
+                Pattern.compile("(?:\\.\\w*)?The last packet successfully received from the server was \\d+ milliseconds ago\\. The last packet sent successfully to the server was \\d+ milliseconds ago\\.(?:\\.\\w*)?")));
         checks.add(new Check("MySQL SSL Error",
                 "https://github\\.com/lucko/LuckPerms/wiki/Storage-system-errors#mysql-ssl-errors",
-                Pattern.compile(("Establishing SSL connection without server's identity verification is not recommended\\."))));
+                Pattern.compile(("(?:\\.\\w*)?Establishing SSL connection without server's identity verification is not recommended\\.(?:\\.\\w*)?"))));
     }
 
 
@@ -62,6 +62,7 @@ public class ErrorListener implements MessageCreateListener {
             // pastebins.get(pastebin) to get the GET URL
             Matcher matcher = pastebin.matcher(event.getMessageContent());
             if (!matcher.matches()) continue;
+            logger.debug(matcher.toString());
             String urlToGet = pastebins.get(pastebin).replace("{code}", matcher.group(1));
             Request request = new Request.Builder()
                 .url(urlToGet)
@@ -76,6 +77,7 @@ public class ErrorListener implements MessageCreateListener {
                     for (Pattern regex : check.getPatterns()) {
                         if (regex.matcher(content).matches())
                             matched = true;
+                        logger.debug(String.valueOf(matched));
                     }
 
                     if (!matched) continue;
