d4fb6903f5685c8fbf634546232e842172e3c346
Larry <larrydblomme@hotmail.com>
2020-06-27 20:53:04 -0600

    make updatelistener listen to more

--- src/main/java/dev/laarryy/clippyv2/listeners/UpdateListener.java
+++ src/main/java/dev/laarryy/clippyv2/listeners/UpdateListener.java
@@ -11,7 +11,7 @@ public class UpdateListener implements MessageCreateListener {
         if (ev.getMessageAuthor().isYourself()) {
             return;
         }
-        if (ev.getMessageContent().contains("1.16") && ev.getMessageContent().contains("update")) {
+        if ((ev.getMessageContent().contains("1.16")) && (ev.getMessageContent().contains("update") || ev.getMessageContent().contains("luckperms") || ev.getMessageContent().contains("lp") || ev.getMessageContent().contains("when") || ev.getMessageContent().contains("luckyperms"))) {
             ev.getChannel().sendMessage("LuckPerms already works on 1.16! Please get the latest version from https://luckperms.net");
         }
         else return;
